564aa6f0d2f9af1d9abcc27e947c1c17b2363749
LantzWeaver <lantz.c.weaver@gmail.com>
2017-06-23 18:19:12 -0400

    psychic

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var Wins
+var Losses
+var Guesses Left
+var Letters Guessed
+var Letters ["a","b", "c", "d", "e", "f", "g", "h",
+ "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+  "t", "u", "v", "w", "x", "y", "z"];

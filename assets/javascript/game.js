9ad37800038e7547451ddab7e2e650fabd1b2617
LantzWeaver <lantz.c.weaver@gmail.com>
2017-06-23 19:05:55 -0400

    6-23

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,20 @@
-
-var Wins
-var Losses
-var Guesses Left
-var Letters Guessed
-var Letters ["a","b", "c", "d", "e", "f", "g", "h",
+var Letters = ["a","b", "c", "d", "e", "f", "g", "h",
  "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
   "t", "u", "v", "w", "x", "y", "z"];
+
+var Wins = 0;
+var Losses = 0;
+var GuessesLeft = 9;
+var LettersGuessed = [];
+var EachGuess = null;
+
+var ComputerGuess = Letters[Math.floor(Math.random() * Letters.length)]
+console.log("Wins:" + Wins + "Losses:" + Losses +"Guesses Left:" + GuessesLeft + "Letters Guessed" + LettersGuessed + "Computer Guess" + ComputerGuess);
+
+document.onkeyup = function(e) {
+	var EachGuess = String.fromCharCode(event.keyCode).toUpperCase();
+
+	if (LettersGuessed.indexOf(EachGuess) < 0 && Letters.indexOf(EachGuess) >= 0) {
+		LettersGuessed[LettersGuessed.length] = EachGuess; GuessesLeft--;
+	}
+}
